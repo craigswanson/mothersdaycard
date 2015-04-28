610839dcccdfa57e81e7c6bbd72a1bcda3d480fb
craigswanson <craigswanson@users.noreply.github.com>
2015-04-28 16:44:25 -0700

    Create send-email.js

---
+++ send-email.js
@@ -0,0 +1,33 @@
+// Create a function to log the response from the Mandrill API
+function log(obj) {
+    $('#response').text(JSON.stringify(obj));
+}
+
+// create a new instance of the Mandrill class with your API key
+var m = new mandrill.Mandrill('ZuU4VKZr5FlKLUdIJaa-1Q');
+
+// create a variable for the API call parameters
+var params = {
+        "template_name": "mother-s-day-card",
+        "template_content": [{
+            "name": "anne_message",
+            "content": "Holy cow! I'm sending my first Mandrill email template via Codecademy!"
+        }],
+
+    "message": {
+        "from_email":"craig@craigswanson.org",
+        "to":[{"email":"craig@craigswanson.org",}],
+        "subject": "Happy Mother's Day from Anne Geddes!",
+        "text": "I'm learning the Mandrill API at Codecademy."
+    }
+};
+
+function sendTheMail() {
+// Send the email!
+
+    m.messages.sendTemplate (params, function(res) {
+        log(res);
+    }, function(err) {
+        log(err);
+    });
+}
